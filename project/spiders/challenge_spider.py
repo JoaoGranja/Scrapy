921e378553887be705d080d12820477af44f48f6
JoaoGranja <joaogranja10@hotmail.com>
2019-03-25 23:34:43 +0000

    Add selected option possibility on formRequest

--- project/spiders/challenge_spider.py
+++ project/spiders/challenge_spider.py
@@ -3,44 +3,50 @@ import scrapy
 
 class ChallengeSpider(scrapy.Spider):
     name = "challenge"
-    first = True
-    count = 0
+    start = True
     result = {}
     
     def start_requests(self):
         url = 'https://www.drukzo.nl.joao.hlop.nl/python.php'
         yield scrapy.Request(url=url, callback=self.parse)
-
-    
         
     def parse(self, response):
         OPTION_SELECTOR = 'option::text'
-        
-        if self.first == False:
+
+        # Get the last option chosen and last drop number
+        if self.start == False:
             last_option = response.meta['item']
-            print("last option = ", last_option, self.count)
+            last_drops  = response.meta['drop_nr']
+            print("last option = ", last_option, last_drops)
         else:
+            self.start = False
+            last_drops = 0
             last_option = None
-            self.first = False
-        
+
+        # Get the drops
         drops = response.xpath('//select[contains(@id, "drop")]')
-        #print("Drop =", drops, self.count)
-        if len(drops) <= self.count:
-                yield {'Decision Tree': self.result}
-        else:        
-            options = drops[self.count].css(OPTION_SELECTOR).getall()
-            print("options = ", options)
-        
-            self.count = self.count + 1
-        
+        if len(drops) == last_drops:
+            # No more drops, so yield
+            yield {'Decision Tree': self.result}
+        else:
+            # Get all options from last drop and iterate over them
+            options = drops[len(drops)-1].css(OPTION_SELECTOR).getall()
+            #if last_option is not None:
+            #    self.result[last_option] = options
+                
             for option in options:
+                if last_option is not None:
+                    self.result[last_option] = option
+                print("options = ", option)
                 request = scrapy.FormRequest.from_response(
                     response,
-                    formdata={'type': 'submit', 'value' :'submit'},
+                    formdata={'type': 'submit', 'value' :'submit', 'selected': option},
                     callback=self.parse)
                 request.meta['item'] = option
-                yield request
-
-                if last_option is not None:
-                    self.result[last_option] = options
+                request.meta['drop_nr'] = len(drops)
+                yield request 
                 
+            
+
+
+
