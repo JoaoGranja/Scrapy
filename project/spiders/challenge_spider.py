4cc4989d52b1055b844d9e7e8d00b66672062730
JoaoGranja <joaogranja10@hotmail.com>
2019-03-25 23:09:43 +0000

    Update challenge_spider.py

--- project/spiders/challenge_spider.py
+++ project/spiders/challenge_spider.py
@@ -1,20 +1,46 @@
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-    ]
+class ChallengeSpider(scrapy.Spider):
+    name = "challenge"
+    first = True
+    count = 0
+    result = {}
+    
+    def start_requests(self):
+        url = 'https://www.drukzo.nl.joao.hlop.nl/python.php'
+        yield scrapy.Request(url=url, callback=self.parse)
 
+    
+        
     def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('span small::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
-            }
+        OPTION_SELECTOR = 'option::text'
+        
+        if self.first == False:
+            last_option = response.meta['item']
+            print("last option = ", last_option, self.count)
+        else:
+            last_option = None
+            self.first = False
+        
+        drops = response.xpath('//select[contains(@id, "drop")]')
+        #print("Drop =", drops, self.count)
+        if len(drops) <= self.count:
+                yield {'Decision Tree': self.result}
+        else:        
+            options = drops[self.count].css(OPTION_SELECTOR).getall()
+            print("options = ", options)
+        
+            self.count = self.count + 1
+        
+            for option in options:
+                request = scrapy.FormRequest.from_response(
+                    response,
+                    formdata={'type': 'submit', 'value' :'submit'},
+                    callback=self.parse)
+                request.meta['item'] = option
+                yield request
 
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+                if last_option is not None:
+                    self.result[last_option] = options
+                
